f6a67ca96e8e841b228a6ca2b26eed0cdf7db66e
Aidan Yang <aidanbyang@hotmail.com>
2018-09-23 19:37:56 -0400

    app is running now but button icons won't show

--- android-dev/app/src/main/java/com/example/aidan/lucyar/MainActivity.java
+++ android-dev/app/src/main/java/com/example/aidan/lucyar/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
@@ -12,31 +13,12 @@ import android.widget.Toast;
 import com.google.ar.core.ArCoreApk;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        maybeEnableArButton();
-
-        /*BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch(item.getItemId()){
-                    case R.id.action_add:
-                        Toast.makeText(MainActivity.this, "Action add clicked", Toast.LENGTH_SHORT).show();
-                        break;
-
-                }
-                return true;
-            }
-        });*/
         BottomNavigationView bottomNavigationView = (BottomNavigationView)
-
                 findViewById(R.id.bottom_navigation);
-
-
 
         bottomNavigationView.setOnNavigationItemSelectedListener(
 
@@ -58,41 +40,9 @@ public class MainActivity extends AppCompatActivity {
                                 break;
                             case R.id.action_search:
                                 break;
-
-
                         }
-
                         return true;
-
                     }
-
                 });
-
-
-
-
     }
-
-    void maybeEnableArButton() {
-        ArCoreApk.Availability availability = ArCoreApk.getInstance().checkAvailability(this);
-        if (availability.isTransient()) {
-            // Re-query at 5Hz while compatibility is checked in the background.
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    maybeEnableArButton();
-                }
-            }, 200);
-        }
-        /*if (availability.isSupported()) {
-            mArButton.setVisibility(View.VISIBLE);
-            mArButton.setEnabled(true);
-            // indicator on the button.
-        } else { // Unsupported or unknown.
-            mArButton.setVisibility(View.INVISIBLE);
-            mArButton.setEnabled(false);
-        }*/
-    }
-
-
 }
